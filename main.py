90417333ee70a8af152eef82a608ade34cd17083
Jackson Antonio do Prado Lima <jacksonpradolima@gmail.com>
2023-09-08 21:50:10 -0300

    chore(main) Update docstring

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from dotenv import load_dotenv
 import coleman4hcs.policy
 import coleman4hcs.reward
 from coleman4hcs.agent import (
-    RewardAgent, 
-    RewardSlidingWindowAgent, 
-    ContextualAgent, 
+    RewardAgent,
+    RewardSlidingWindowAgent,
+    ContextualAgent,
     SlidingWindowContextualAgent
 )
 from coleman4hcs.environment import Environment
 from coleman4hcs.evaluation import NAPFDVerdictMetric
 from coleman4hcs.policy import FRRMABPolicy, SWLinUCBPolicy, LinUCBPolicy
 from coleman4hcs.scenarios import (
-    IndustrialDatasetHCSScenarioProvider, 
+    IndustrialDatasetHCSScenarioProvider,
     IndustrialDatasetScenarioProvider,
     IndustrialDatasetContextScenarioProvider
 )
@@ -31,18 +31,17 @@ def exp_run_industrial_dataset(iteration, trials, env: Environment, experiment_d
     """
     Execute a single run of the industrial dataset experiment.
 
-    This function runs the experiment for a given iteration and saves the results 
-    in the specified directory. The results are stored in a CSV format.
-
-    Parameters:
-    - iteration (int): The current iteration of the experiment.
-    - trials (int): The total number of trials to be executed.
-    - env (Environment): An instance of the environment where the experiment is run.
-    - experiment_directory (str): The directory where experiment results will be stored.
-
-    Returns:
-    None
+    :param iteration: The current iteration of the experiment.
+    :type iteration: int
+    :param trials: The total number of trials to be executed.
+    :type trials: int
+    :param env: An instance of the environment where the experiment is run.
+    :type env: Environment
+    :param experiment_directory: The directory where experiment results will be stored.
+    :type experiment_directory: str
+    :return: None
     """
+
     env.run_single(iteration, trials, print_log=True)
     env.store_experiment(f"{experiment_directory}{str(env.scenario_provider)}.csv")
 
@@ -51,34 +50,35 @@ def load_class_from_module(module, class_name: str):
     """
     Dynamically loads a class from a given module.
 
-    Parameters:
-    - module (module): The Python module from which to load the class.
-    - class_name (str): The name of the class to be loaded.
-
-    Returns:
-    - class: The loaded class.
-
-    Raises:
-    - ValueError: If the class is not found in the provided module.
+    :param module: The Python module from which to load the class.
+    :type module: module
+    :param class_name: The name of the class to be loaded.
+    :type class_name: str
+    :return: The loaded class.
+    :rtype: class
+    :raises ValueError: If the class is not found in the provided module.
     """
+
     if hasattr(module, class_name):
         return getattr(module, class_name)
     else:
         raise ValueError(f"Class '{class_name}' not found in {module.__name__}!")
 
 
-def create_agents_for_policy(policy, rew_fun, window_sizes):
+def create_agents(policy, rew_fun, window_sizes):
     """
     Create agent instances based on the policy type.
 
-    Parameters:
-    - policy (Policy): The policy instance.
-    - rew_fun (RewardFunction): The reward function instance.
-    - window_sizes (list): List of window sizes (only relevant for policies 
-    that use Sliding Window such as FRRMABPolicy).
+    :param policy: The policy instance.
+    :type policy: Policy
+    :param rew_fun: The reward function instance.
+    :type rew_fun: RewardFunction
+    :param window_sizes: List of window sizes (only relevant for policies
+                         that use Sliding Window such as FRRMABPolicy).
+    :type window_sizes: list
 
-    Returns:
-    - list: A list of agent instances.
+    :return: A list of agent instances.
+    :rtype: list
     """
 
     if isinstance(policy, FRRMABPolicy):
@@ -90,28 +90,36 @@ def create_agents_for_policy(policy, rew_fun, window_sizes):
     return [RewardAgent(policy, rew_fun)]
 
 
-def get_scenario(datasets_dir, dataset, sched_time_ratio, use_hcs, use_context, context_config, feature_groups):
+def get_scenario_provider(datasets_dir,
+                          dataset,
+                          sched_time_ratio,
+                          use_hcs,
+                          use_context,
+                          context_config,
+                          feature_groups):
     """
     Return the appropriate scenario provider based on the given configuration.
 
-    The function selects the scenario provider based on whether the 
-    HCS (Highly-Configurable System) configuration is used. 
+    The function selects the scenario provider based on whether the
+    HCS (Highly-Configurable System) configuration is used.
     It constructs the appropriate paths for the
     dataset files and initializes the scenario provider with these paths.
 
-    Parameters:
-    - datasets_dir (str): The directory where datasets are stored.
-    - dataset (str): The specific dataset to be used.
-    - sched_time_ratio (float): The ratio of scheduled time to be used in the scenario.
-    - use_hcs (bool): If True, returns an `IndustrialDatasetHCSScenarioProvider` instance. 
-                      Otherwise, returns an `IndustrialDatasetScenarioProvider` instance.
+    :param datasets_dir: The directory where datasets are stored.
+    :type datasets_dir: str
+    :param dataset: The specific dataset to be used.
+    :type dataset: str
+    :param sched_time_ratio: The ratio of scheduled time to be used in the scenario.
+    :type sched_time_ratio: float
+    :param use_hcs: If True, returns an `IndustrialDatasetHCSScenarioProvider` instance.
+                    Otherwise, returns an `IndustrialDatasetScenarioProvider` instance.
+    :type use_hcs: bool
 
-    Returns:
-    - scenario (IndustrialDatasetScenarioProvider or IndustrialDatasetHCSScenarioProvider):
-      An instance of the scenario provider based on the given configuration.
+    :return: An instance of the scenario provider based on the given configuration.
+    :rtype: IndustrialDatasetScenarioProvider or IndustrialDatasetHCSScenarioProvider
     """
     base_args = [f"{datasets_dir}/{dataset}/features-engineered.csv", sched_time_ratio]
-    
+
     if use_hcs and not use_context:
         scenario_cls = IndustrialDatasetHCSScenarioProvider
         base_args.insert(1, f"{datasets_dir}/{dataset}/data-variants.csv")
@@ -120,7 +128,7 @@ def get_scenario(datasets_dir, dataset, sched_time_ratio, use_hcs, use_context, 
         base_args[0] = f"{datasets_dir}/{dataset}/features-engineered-contextual.csv"
         base_args.insert(1, feature_groups['feature_group_name'])
         base_args.insert(2, feature_groups['feature_group_values'])
-        base_args.insert(3, context_config['previous_build'])    
+        base_args.insert(3, context_config['previous_build'])
     elif use_hcs and use_context:
         raise NotImplementedError
     else:
@@ -142,7 +150,7 @@ if __name__ == '__main__':
         'independent_executions'
     ])
 
-    # Experiment Configuration                                    
+    # Experiment Configuration
     (
         sched_time_ratio,
         datasets_dir,
@@ -165,7 +173,7 @@ if __name__ == '__main__':
     # HCS Configuration
     use_hcs = config.get('hcs_configuration', False).get('wts_strategy', False)
 
-    # Contextual Information                                    
+    # Contextual Information
     (
         context_config,
         feature_groups
@@ -190,13 +198,13 @@ if __name__ == '__main__':
         agent
         for policy_name, reward_policies in policies.items()
         for reward_name, policy in reward_policies.items()
-        for agent in create_agents_for_policy(
+        for agent in create_agents(
             policy,
             load_class_from_module(coleman4hcs.reward, reward_name + "Reward")(),
             algorithm_configs.get(policy_name.lower(), {}).get('window_sizes', [])
         )
     ]
-    
+
     # Check if there's an agent of type SlidingWindowContextualAgent or ContextualAgent
     has_sliding_window_contextual_agent = any(isinstance(agent, SlidingWindowContextualAgent) for agent in agents)
     has_contextual_agent = any(isinstance(agent, ContextualAgent) for agent in agents)
@@ -211,7 +219,8 @@ if __name__ == '__main__':
         Path(experiment_directory).mkdir(parents=True, exist_ok=True)
 
         for dataset in datasets:
-            scenario = get_scenario(datasets_dir, dataset, tr, use_hcs, use_context, context_config, feature_groups)
+            scenario = get_scenario_provider(datasets_dir, dataset, tr, use_hcs, use_context, context_config,
+                                             feature_groups)
 
             # Stop conditional
             trials = scenario.max_builds
